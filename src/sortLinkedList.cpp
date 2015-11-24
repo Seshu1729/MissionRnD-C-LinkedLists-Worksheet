cdc64a829e732e09ce2d054974ddcfe8a4c362bb
Seshu1729 <seshu1729@gmail.com>
2015-11-24 20:15:53 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -1,4 +1,3 @@
-
 /*
 OVERVIEW: Given an unsorted single linked list, sort the linked list without using extra array or linked list.
 E.g.: 4->2->1->5->3, output is 1->2->3->4->5.
@@ -10,7 +9,6 @@ OUTPUT: Sort the linked list.
 ERROR CASES: Return NULL for error cases.
 
 NOTES: Without using extra array or linked list.
-
 */
 
 #include <stdio.h>
@@ -24,11 +22,11 @@ struct node
 
 struct node * sortLinkedList(struct node *head)
 {
-	struct node *nodei,*nodej,*sortCheck=NULL;
+	struct node *nodei, *nodej, *sortCheck = NULL;
 	int temp;
 	if (head == NULL)
 		return NULL;
-	for (nodei = head; nodei->next!=NULL; nodei = nodei->next)
+	for (nodei = head; nodei->next != NULL; nodei = nodei->next)
 	{
 		for (nodej = head; nodej->next != sortCheck; nodej = nodej->next)
 		{
@@ -46,6 +44,44 @@ struct node * sortLinkedList(struct node *head)
 
 /*
 METHOD 2:
+int getLength(struct node *head)
+{
+	int length = 0;
+	while (head != NULL)
+	{
+		head = head->next;
+		length++;
+	}
+	return length;
+}
+
+struct node * sortLinkedList(struct node *head)
+{
+	struct node *tempHead = head,*nodei,*nodej;
+	int length,i,j,temp;
+	if (head == NULL)
+		return NULL;
+	length = getLength(tempHead);
+	for (nodei = head, i = 0; i < length; i++)
+	{
+		for (nodej = head, j = 0; j < length - i - 1; j++)
+		{
+			if (nodej->num > nodej->next->num)
+			{
+				temp = nodej->next->num;
+				nodej->next->num = nodej->num;
+				nodej->num = temp;
+			}
+			nodej = nodej->next;
+		}
+		nodei = nodei->next;
+	}
+	return head;
+}
+
+
+
+METHOD 3:
 Actually it violates basic condions of given problem
 
 void heapUp(int *heap, int index)
